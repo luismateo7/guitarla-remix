b99eb80df529e036853326ea1b19a25ae1d7e8cb
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-15 19:31:06 -0500

    Mostrar las guitarras desde la API

---
+++ app/routes/tienda.jsx
@@ -0,0 +1,49 @@
+import { useLoaderData } from "@remix-run/react"
+import { getGuitarras } from '~/models/guitarra.server'
+import Guitarra from "~/components/guitarra"
+import styles from '~/styles/guitarras.css'
+
+export function meta(){
+  return{
+    title: 'GuitarLA - Tienda de Guitarras',
+    description: 'GuitarLA - Nuestra colección de guitarras'
+  }
+}
+
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export async  function loader(){
+  const guitarras = await getGuitarras()
+  return guitarras.data
+}
+
+const Tienda = () => {
+
+  const guitarras = useLoaderData();
+  console.log(guitarras);
+
+  return (
+    <main className="contenedor">
+      <h2 className="heading">Nuestra Colección</h2>
+      {guitarras?.length && (
+        <div className="guitarras-grid">
+          {guitarras.map( guitarra => (
+            <Guitarra 
+              key={guitarra?.id}
+              guitarra={guitarra?.attributes}
+            />
+          ))}
+        </div>
+      )}
+    </main>
+  )
+}
+
+export default Tienda
