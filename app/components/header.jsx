0e7eb6683f66a250ebbf0d72d61df0ce21557596
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-16 02:51:24 -0500

    Header principal inicial

---
+++ app/components/header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "@remix-run/react"
+import logo from '../../public/img/logo.svg'
+import Navegacion from "./navegacion"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="contenedor barra">
+                <Link to='/'>
+                    <img className="logo" src={logo} alt="Logo GuitarLA" />
+                </Link>
+                <Navegacion />
+            </div>
+        </header>
+    )
+}
+
+export default Header
