0bb88c517728c71e57a3f93015464b922ee4014a
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-21 03:05:42 -0500

    Get a la API para obtener una entrada específica

--- app/models/post.server.js
+++ app/models/post.server.js
@@ -3,3 +3,9 @@ export async function getPosts(){
     const resultado = await respuesta.json()
     return resultado;
 }
+
+export async function getPost(post){
+    const respuesta = await fetch(`${process.env.API_URL}/posts?filters[url]=${post}&populate=imagen`)
+    const resultado = await respuesta.json()
+    return resultado;
+}
