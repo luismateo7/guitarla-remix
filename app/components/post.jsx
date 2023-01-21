4d76c2bf48e2f00d505128065c27d606bf18279b
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-21 03:11:51 -0500

    Render entradas del blog que vienen desde la API

---
+++ app/components/post.jsx
@@ -0,0 +1,20 @@
+import { Link } from "@remix-run/react"
+import { formatearFecha } from "~/utils/helpers";
+
+const Post = ({post})=> {
+    const { imagen, contenido, titulo, url, publishedAt} = post
+
+    return (
+        <article className="post">
+            <img src={imagen.data[0].attributes.formats.small.url} alt={`Imagen de ${titulo}`} className="imagen" />
+            <div className="contenido">
+                <h3>{titulo}</h3>
+                <p className="fecha">{formatearFecha(publishedAt)}</p>
+                <p className="resumen">{contenido}</p>
+                <Link className="enlace" to={`/post/${url}`}>Leer Post</Link>
+            </div>
+        </article>
+    )
+}
+
+export default Post
