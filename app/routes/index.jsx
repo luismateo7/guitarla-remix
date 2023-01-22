0b7a122e6506b5dda6738d68f10c108cac52ddcc
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-22 17:54:47 -0500

    Mostrar Listado de guitarras, entradas y el curso

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,77 @@
+import { useLoaderData } from '@remix-run/react';
+
+import { getGuitarras } from '~/models/guitarra.server'
+import { getPosts } from '~/models/post.server'
+import { getCurso } from '~/models/cursos.server'; 
+
+import Curso from '~/components/curso';
+
+import ListadoGuitarras from '~/components/listado-guitarras';
+import ListadoPosts from '~/components/listado-posts';
+
+import stylesGuitarras from '~/styles/guitarras.css'
+import stylesEntradas from "~/styles/blog.css"
+import stylesCurso from "~/styles/curso.css"
+// export function meta(){
+
+// }
+
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: stylesGuitarras
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesEntradas
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesCurso
+    }
+  ]
+}
+
+export async function loader(){
+
+  const [guitarras, posts, curso] = await Promise.all([
+    getGuitarras(),
+    getPosts(),
+    getCurso()
+  ])
+
+
+  return {
+    guitarras: guitarras.data,
+    posts: posts.data,
+    curso: curso.data
+  }
+
+}
 
 const Index = () => {
+
+  const { guitarras, posts, curso } = useLoaderData()
+
   return (
-    <h1>Desde Remix JS</h1>
+    <>
+      <main className="contenedor">
+        <ListadoGuitarras
+          guitarras={guitarras}
+        />
+      </main>
+
+      <Curso
+        curso={curso.attributes}
+      />
+
+      <section className='contenedor'>
+        <ListadoPosts
+          posts={posts}
+        />
+      </section>
+    </>
   )
 }
 
