b7fc8433d344b564c322529daa32bf232be67b61
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-21 22:35:12 -0500

    Update Tienda a Guitarras & Add Carrito

--- app/components/navegacion.jsx
+++ app/components/navegacion.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "@remix-run/react"
+import imagenCarrito from "../../public/img/carrito.png"
 
 const Navegacion = () => {
 
@@ -8,8 +9,11 @@ const Navegacion = () => {
         <nav className="navegacion">
             <Link to='/' className={location.pathname === '/' ? 'active' : ''}>Inicio</Link>
             <Link to='/nosotros' className={location.pathname === '/nosotros' ? 'active' : ''}>Nosotros</Link>
-            <Link to='/tienda' className={location.pathname === '/tienda' ? 'active' : ''}>Tienda</Link>
+            <Link to='/guitarras' className={location.pathname === '/guitarras' ? 'active' : ''}>Tienda</Link>
             <Link to='/blog' className={location.pathname === '/blog' ? 'active' : ''}>Blog</Link>
+            <Link to='/carrito'>
+                <img src={imagenCarrito} alt="carrito de compras" />
+            </Link>
         </nav>
     )
 }
